0ca7793311a701256f4d6bc0e086a3d35beb5db1
Vis'Eyth <viseyth@proton.me>
2025-09-03 21:55:02 +0200

    ext::ctx::SubscribeAsync: await subscriptions, patch sdk for IntoQueries

--- src/ext/ctx.rs
+++ src/ext/ctx.rs
@@ -1,4 +1,6 @@
-use crate::{global, region, sdk::{DbContext, Result}};
+use crate::{global, region, sdk::{DbContext, IntoQueries, Result}};
+use crate::sdk::__codegen::{SpacetimeModule, SubscriptionBuilder};
+use tokio::sync::mpsc::unbounded_channel as channel;
 
 async fn run_until(
     ctx: &impl DbContext,
@@ -26,5 +28,27 @@ impl RunUntil for global::DbConnection {
 impl RunUntil for region::DbConnection {
     async fn run_until(self, signal: impl Future) -> Result<()> {
         run_until(&self, self.run_async(), signal).await
+    }
+}
+
+
+pub trait SubscribeAsync<M: SpacetimeModule> {
+    fn subscribe_async<Queries: IntoQueries>(self, queries: Queries) -> impl Future<Output = Result<M::SubscriptionHandle>>;
+}
+
+impl<M: SpacetimeModule> SubscribeAsync<M> for SubscriptionBuilder<M> {
+    async fn subscribe_async<Queries: IntoQueries>(mut self, queries: Queries) -> Result<M::SubscriptionHandle> {
+        let (tx, mut rx) = channel();
+        {
+            let tx = tx.clone();
+            self = self.on_error(move |_, e| { let _ = tx.send(Err(e)); });
+        }
+        {
+            let tx = tx.clone();
+            self = self.on_applied(move |_| { let _ = tx.send(Ok(())); });
+        }
+        let handle = self.subscribe(queries);
+        rx.recv().await.unwrap()?;
+        Ok(handle)
     }
 }
